ff516fe2eea435ce5707a16797d5f5d27d7c4bd2
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-03-03 15:54:08 -0500

    Scaffold the app

--- app.py
+++ app.py
@@ -3,7 +3,8 @@
 # external libraries
 from sanic import Sanic
 from sanic.views import HTTPMethodView
-from sanic.response import text
+from sanic.response import json
+from uuid import uuid4
 
 app = Sanic()
 
@@ -14,26 +15,72 @@ GET /task/<id>
 PUT /task/<id>
 """
 
-class Oscar(HTTPMethodView):
-    def get(self, request, task):
-        return text('I am get method')
 
-    def post(self, request, task):
-        return text('I am post method')
+DATOS = {}
 
-    def put(self, request, task):
-        return text('I am put method')
 
 class TaskList(HTTPMethodView):
+
     def post(self, request):
         """
         Create a task out of the post json body.
         """
+        print(request.json)
+        host = request.headers.get('Host')
+        uid = uuid4().hex
+        task = {
+            'id': uid,
+            'title': 'Comprar leche',
+            'done': False,
+            'url': f'http://{host}/tasks/{uid}'
+        }
+        DATOS.update({uid: task})
+        return json(task)
+
+    def get(self, request):
+        """
+        Lists all the tasks available, could support pagination.
+        """
         return json({
+            'data': DATOS,
         })
 
 
-app.add_route(Oscar.as_view(), '/<task>')
+class Task(HTTPMethodView):
+
+    def get(self, request, task):
+        """
+        Shows the details of an individual task.
+        """
+        task = DATOS.get(task)
+        if task is None:
+            return json({
+                'error': {
+                    'code': 'TASK_NOT_FOUND',
+                    'message': 'That task was not found',
+                }
+            }, status=404)
+        return json(task)
+
+    def put(self, request, task):
+        task = DATOS.get(task)
+        if task is None:
+            return json({
+                'error': {
+                    'code': 'TASK_NOT_FOUND',
+                    'message': 'That task was not found',
+                }
+            }, status=404)
+        print(request.json)
+        task.update({
+            'done': True
+        })
+        DATOS.update({task['id']: task})
+        return json(task)
+
+
+app.add_route(TaskList.as_view(), '/tasks')
+app.add_route(Task.as_view(), '/tasks/<task>')
 
 if __name__ == '__main__':
     app.run(
